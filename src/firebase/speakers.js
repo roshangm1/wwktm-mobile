5e64acb2a9df895ffbc2a8e10959c9daf5aa32e9
Aawaz Gyawali <awazgyawali@gmail.com>
2019-08-21 23:36:37 +0545

    Speaker function added

--- src/firebase/speakers.js
+++ src/firebase/speakers.js
@@ -4,3 +4,7 @@ export async function getAllSpeakers() {
   const speakersList = await fireStoreRef.collection('speakers').get();
   return speakersList.docs.map(speaker => speaker.data());
 }
+
+export async function getSpeaker(speakerKey) {
+  return (await fireStoreRef.collection('speakers').doc(speakerKey).get()).data()
+}
