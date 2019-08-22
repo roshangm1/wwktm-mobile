39b88b30597cc3b2f9c8f4fee931ef63f21b7e13
Aawaz Gyawali <awazgyawali@gmail.com>
2019-08-22 09:13:30 +0545

    Schedule adding functoin added

--- src/firebase/schedule.js
+++ src/firebase/schedule.js
@@ -4,3 +4,20 @@ export async function getProgramSchedule() {
   const scheduleData = await fireStoreRef.collection('schedule').get();
   return scheduleData.docs.map(schedule => schedule.data());
 }
+
+export async function addNewSchedule(scheduleDetail) {
+  // Todo: Remove this once the function is properly used with object containing these fields
+  let {
+    name,
+    startTime,
+    endTime,
+    description,
+    speakerId,
+    // Type here refers to different types of session, probably break, talk, game, outdoor activity etc
+    type,
+  } = scheduleDetail;
+
+  let ref = fireStoreRef.collection('schedule').doc();
+  scheduleDetail.id = ref.id;
+  await ref.set(scheduleDetail);
+}
