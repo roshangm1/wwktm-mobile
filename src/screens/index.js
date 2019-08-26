17d6d1053357370c6da292e79550d60ae127b143
Roshan Gautam <roosan.gm@gmail.com>
2019-08-26 20:14:29 +0545

    make folder name start with small letter

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,9 +1,9 @@
-export { default as Home } from './Home/index';
-export { default as Speakers } from './Speakers';
-export { default as SpeakerDetail } from './Speakers/SpeakerDetail';
+export { default as Home } from './home/index';
+export { default as Speakers } from './speakers';
+export { default as SpeakerDetail } from './speakers/SpeakerDetail';
 
-export { default as Schedule } from './Schedule/Schedule';
-export { default as ScheduleDetail } from './Schedule/ScheduleDetail';
+export { default as Schedule } from './schedule/Schedule';
+export { default as ScheduleDetail } from './schedule/ScheduleDetail';
 
-export { default as Login } from './Auth/Login';
-export { default as Register } from './Auth/Register';
+export { default as Login } from './auth/Login';
+export { default as Register } from './auth/Register';
