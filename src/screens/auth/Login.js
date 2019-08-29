e57e558e1b5096e9b231db783eb5a95a700c7740
rashila.p <rashila.p@arbyte.com.np>
2019-08-29 15:38:47 +0545

    ....

--- src/screens/auth/Login.js
+++ src/screens/auth/Login.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
 import { Card, TextInput, Button } from 'react-native-paper';
 import { loginWithEmailAsync } from '../../firebase/auth';
 
@@ -7,7 +7,6 @@ const Login = ({ navigation }) => {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const enterPassword = React.createRef();
 
   const loginWithEmail = async () => {
     setLoading(true);
@@ -17,35 +16,36 @@ const Login = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.rootContainer}>
+    <View style={{ flex: 1, justifyContent: 'center' }}>
       <Card elevation={2} style={{ margin: 16 }}>
         <Card.Content>
-          <Text style={styles.titleText}>Login</Text>
+          <Text
+            style={{ fontWeight: 'bold', fontSize: 18, alignSelf: 'center' }}
+          >
+            Login
+          </Text>
           <TextInput
             label="Email"
             mode="outlined"
-            style={styles.inputText}
+            style={{ marginTop: 8 }}
             value={email}
             autoCapitalize="none"
-            returnKeyType="next"
             onChangeText={text => setEmail(text)}
-            onSubmitEditing={() => enterPassword.current.focus()}
           />
           <TextInput
-            ref={enterPassword}
             label="Password"
             mode="outlined"
             value={password}
             secureTextEntry
             onChangeText={text => setPassword(text)}
-            style={styles.inputText}
+            style={{ marginTop: 8 }}
           />
           <Button
             dark
             mode="contained"
             loading={loading}
             onPress={loginWithEmail}
-            style={styles.button}
+            style={{ marginTop: 16 }}
           >
             Log In
           </Button>
@@ -54,24 +54,5 @@ const Login = ({ navigation }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  titleText: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    alignSelf: 'center',
-  },
-  button: {
-    marginTop: 16,
-    paddingVertical: 5,
-  },
-  inputText: {
-    marginTop: 8,
-  },
-});
 
 export default Login;
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
+import { logout } from '../../firebase/auth';
 import MainLayout from '../MainLayout';
 
 const Home = ({ navigation }) => {
+  const logoutUser = () => {
+    logout();
+    navigation.navigate('AuthStack');
+  };
   return (
     <MainLayout title="Home">
-      <View style={styles.mainContainer}>
-        <Text> Welcome to Web Weekend Kathmandu mobile app</Text>
+      <View>
+        <Text onPress={logoutUser}>Logout</Text>
       </View>
     </MainLayout>
   );
 };
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
 export default Home;
