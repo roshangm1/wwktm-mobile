f0fc60ac6bcf80a07d0a2744d069f0eeaede1cdf
rashila.p <rashila.p@arbyte.com.np>
2019-08-31 23:41:17 +0545

    post item component created

--- src/screens/activity/PostItem.js
+++ src/screens/activity/PostItem.js
@@ -1,22 +1,100 @@
 import React from 'react';
-import { Text, Image } from 'react-native';
-import { Card } from 'react-native-paper';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import Row from '../../components/Row';
+import ActionButton from '../../components/ActionButton';
+import { Avatar } from 'react-native-paper';
+import { getPostTime } from '../../utils/date';
 
-const PostItem = ({ feed }) => (
-  <Card style={{ margin: 8 }}>
-    <Card.Content>
-      <Text style={{ fontWeight: 'bold' }}>{feed.name}</Text>
-      <Text>{feed.content}</Text>
-      {feed.postImage && (
+const PostItem = ({ feed }) => {
+  const getNameInitials = name => {
+    var nameInitials = name.match(/\b(\w)/g).join(' ');
+    return nameInitials;
+  };
+  const {
+    profileImageUrl,
+    postImage,
+    content,
+    name,
+    likesCount,
+    commentsCount,
+    date,
+  } = feed;
+  return (
+    <View style={styles.rootContainer}>
+      <Row>
+        {profileImageUrl ? (
+          <Avatar.Image
+            size={36}
+            source={{
+              uri: profileImageUrl,
+            }}
+          />
+        ) : (
+          <Avatar.Text size={36} label={getNameInitials(name)} />
+        )}
+
+        <View style={{ paddingHorizontal: 5 }}>
+          <Text>{name}</Text>
+          <Text style={styles.dateText}>{getPostTime(date)}</Text>
+        </View>
+      </Row>
+      <Text style={styles.descriptionText}>{content}</Text>
+      {postImage && (
         <Image
-          source={{ uri: feed.postImage }}
-          style={{
-            height: 200,
+          style={styles.imageStyle}
+          source={{
+            uri: postImage,
           }}
         />
       )}
-    </Card.Content>
-  </Card>
-);
+      <Row style={styles.bottomRowContainer}>
+        {likesCount && <Text style={styles.countText}>5 Likes</Text>}
+        {commentsCount && <Text style={styles.countText}>10 Comment</Text>}
+      </Row>
+      <Row style={styles.bottomRowContainer}>
+        <ActionButton
+          iconName="heart"
+          title="Like"
+          onPress={() => alert('Like Pressed')}
+        />
+        <ActionButton
+          iconName="comment"
+          title="Comment"
+          onPress={() => alert('Comment Pressed')}
+        />
+      </Row>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderColor: '#808080',
+    borderWidth: 1,
+    marginTop: 10,
+  },
+  dateText: {
+    color: '#808080',
+    paddingTop: 3,
+  },
+  descriptionText: {
+    paddingTop: 10,
+  },
+  bottomRowContainer: {
+    paddingTop: 7,
+  },
+  countText: {
+    fontSize: 14,
+    color: '#808080',
+    paddingRight: 10,
+  },
+  imageStyle: {
+    height: 300,
+    width: '100%',
+    marginTop: 10,
+  },
+});
 
 export default PostItem;
