e34ed9b765a5255fc0ca0a5e1d30e65d212e4a96
Roshan Gautam <roosan.gm@gmail.com>
2019-09-03 17:58:35 +0545

    fix google signin

---
+++ src/screens/activity/PostDetail.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native';
+import MainLayout from '../../layouts/MainLayout';
+import PostItem from './PostItem';
+import {
+  upvoteFeed,
+  addCommentToPost,
+  getCommentsForPost,
+} from '../../firebase/activity';
+import Colors from '../../configs/colors';
+import { FlatList } from 'react-native-gesture-handler';
+import { TouchableRipple } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import CommentItem from './CommentItem';
+
+const PostDetail = ({ navigation }) => {
+  const { post } = navigation.state.params;
+  const [comments, setComments] = useState(null);
+  const [comment, setComment] = useState('');
+  const inputRef = useRef();
+
+  const likePost = () => {
+    upvoteFeed(post);
+  };
+  const onComment = () => {
+    addCommentToPost(post.id, comment);
+    inputRef.current.clear();
+  };
+
+  const updateComments = response => {
+    setComments(response);
+  };
+
+  const handleInputChange = text => {
+    setComment(text);
+  };
+
+  const renderComment = ({ item }) => {
+    return <CommentItem comment={item} />;
+  };
+  const renderAllComments = () => {
+    if (comments === null) {
+      return <Text style={{ textAlign: 'center' }}>Loading...</Text>;
+    }
+    if (comments.length <= 0) {
+      return <Text style={{ textAlign: 'center' }}>No comments</Text>;
+    }
+    return (
+      <FlatList
+        data={comments}
+        renderItem={renderComment}
+        keyExtractor={item => item.id}
+      />
+    );
+  };
+
+  useEffect(() => {
+    getCommentsForPost(post.id, updateComments);
+  }, [post.id]);
+  return (
+    <MainLayout title="Details" icon="arrow-left">
+      <ScrollView>
+        <KeyboardAvoidingView behavior="padding">
+          <PostItem
+            post={post}
+            onLikePress={likePost}
+            onCommentPress={() => console.log('comments')}
+          />
+          <View style={styles.commentsContainer}>
+            <Text style={styles.commentsTitle}>Comments</Text>
+            {renderAllComments()}
+          </View>
+
+          <View
+            style={{
+              flexDirection: 'row',
+              marginVertical: 8,
+              borderColor: Colors.grey,
+              borderWidth: 1,
+              borderRadius: 6,
+              padding: 4,
+              marginHorizontal: 16,
+            }}
+          >
+            <TextInput
+              ref={inputRef}
+              placeholder="What's on your mind?"
+              style={{ flex: 1 }}
+              value={comment}
+              onChangeText={handleInputChange}
+            />
+            <TouchableRipple
+              style={{
+                paddingHorizontal: 15,
+                justifyContent: 'center',
+              }}
+              onPress={onComment}
+            >
+              <Icon name="send" size={20} color={Colors.black} />
+            </TouchableRipple>
+          </View>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </MainLayout>
+  );
+};
+
+const styles = StyleSheet.create({
+  commentsContainer: {
+    paddingHorizontal: 16,
+  },
+  commentsTitle: {
+    color: Colors.lightBlack,
+  },
+});
+export default PostDetail;
