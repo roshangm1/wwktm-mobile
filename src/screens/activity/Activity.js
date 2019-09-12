9a31387ce9fac66d478f06d18c8b0002d70e086b
rashila.p <rashila.p@arbyte.com.np>
2019-09-12 12:03:19 +0545

    notification workFlow

--- src/screens/activity/Activity.js
+++ src/screens/activity/Activity.js
@@ -1,5 +1,5 @@
 import { FlatList, TextInput, StyleSheet } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { TouchableRipple } from 'react-native-paper';
 
 import PostItem from './PostItem';
@@ -10,9 +10,15 @@ import Colors from '../../configs/colors';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import Spinner from '../../components/Spinner';
 import EmptyComponent from './../../components/EmptyComponent';
+import firebase from 'react-native-firebase';
 
 const Activity = ({ navigation }) => {
   const [feed, setFeed] = useState(null);
+  const [notificationListener, setNotificationListener] = useState();
+  const [
+    notificationOpenedListener,
+    setNotificationOpenedListener,
+  ] = useState();
 
   const updateFeed = response => {
     setFeed(response);
@@ -27,7 +33,72 @@ const Activity = ({ navigation }) => {
 
   useEffect(() => {
     getFeedData(updateFeed);
+    handleNotification();
+    return () => {
+      notificationListener();
+      notificationOpenedListener();
+    };
+  }, [handleNotification, notificationListener, notificationOpenedListener]);
+
+  const handlePermission = async () => {
+    const token = await firebase.messaging().getToken();
+    // await registerDeviceId(token);
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      // user has permissions
+    } else {
+      try {
+        await firebase.messaging().requestPermission();
+        // User has authorised
+      } catch (error) {
+        // User has rejected permissions
+      }
+    }
+  };
+
+  const handleNotification = useCallback(() => {
+    handlePermission();
+    setNotificationListener(
+      firebase.notifications().onNotification(async notification => {
+        const channel = new firebase.notifications.Android.Channel(
+          'rnc',
+          'rnc',
+          firebase.notifications.Android.Importance.Max,
+        );
+        firebase.notifications().android.createChannel(channel);
+        const notif = new firebase.notifications.Notification()
+          .setNotificationId('3')
+          .setTitle(notification.data.title)
+          .setBody(notification.data.body)
+          .setData(notification.data)
+          .android.setChannelId('rnc');
+
+        firebase.notifications().displayNotification(notif);
+      }),
+    );
+
+    // check the initial opening notification (when closed state)
+    firebase
+      .notifications()
+      .getInitialNotification()
+      .then(notifOpen => {
+        handleNotificationClick(notifOpen);
+      })
+      .catch(err => console.log(err));
+
+    // listen to new notification opens
+    setNotificationOpenedListener(
+      firebase.notifications().onNotificationOpened(notifOpen => {
+        handleNotificationClick(notifOpen);
+      }),
+    );
   }, []);
+
+  const handleNotificationClick = async notifOpen => {
+    if (notifOpen) {
+      const { notification } = notifOpen;
+    }
+  };
 
   const renderItem = ({ item }) => {
     return (
