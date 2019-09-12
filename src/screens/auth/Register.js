51aa98b2ffe94a40306365d952ff1df375b23f1a
Roshan Gautam <roosan.gm@gmail.com>
2019-09-13 02:13:34 +0545

    login with fb error fixed, ui fixes

--- src/screens/auth/Register.js
+++ src/screens/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Toast from 'react-native-simple-toast';
-import { Text, ScrollView } from 'react-native';
+import { Text, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Button, TextInput, Card } from 'react-native-paper';
 
 import AuthLayout from '../../layouts/AuthLayout';
@@ -33,61 +33,67 @@ const Register = ({ navigation }) => {
       <ScrollView
         contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
       >
-        <Card elevation={2} style={{ margin: 16 }}>
-          <Card.Content>
-            <Text
-              style={{ fontWeight: 'bold', fontSize: 18, alignSelf: 'center' }}
-            >
-              Sign Up
-            </Text>
-            <TextInput
-              label="Name"
-              mode="outlined"
-              style={{ marginTop: 8 }}
-              value={signupData.name}
-              autoCapitalize="none"
-              autoCorrect={false}
-              onChangeText={text => handleInputChange('name', text)}
-            />
-            <TextInput
-              label="Email"
-              mode="outlined"
-              keyboardType="email-address"
-              autoCapitalize="none"
-              autoCorrect={false}
-              value={signupData.email}
-              onChangeText={text => handleInputChange('email', text)}
-              style={{ marginTop: 8 }}
-            />
-            <TextInput
-              label="Password"
-              mode="outlined"
-              value={signupData.password}
-              secureTextEntry
-              onChangeText={text => handleInputChange('password', text)}
-              style={{ marginTop: 8 }}
-            />
-            <Button
-              dark
-              mode="contained"
-              loading={loading}
-              onPress={signupUser}
-              style={{ marginTop: 16 }}
-            >
-              Sign Up
-            </Button>
-            <Text
-              onPress={() => navigation.navigate('Login')}
-              style={{
-                textAlign: 'center',
-                marginTop: 16,
-                textDecorationLine: 'underline',
-              }}
-            >
-              Already have an account ? Sign In.
-            </Text>
-          </Card.Content>
-        </Card>
+        <KeyboardAvoidingView behavior="padding">
+          <Card elevation={2} style={{ margin: 16 }}>
+            <Card.Content>
+              <Text
+                style={{
+                  fontWeight: 'bold',
+                  fontSize: 18,
+                  alignSelf: 'center',
+                }}
+              >
+                Sign Up
+              </Text>
+              <TextInput
+                label="Name"
+                mode="outlined"
+                style={{ marginTop: 8 }}
+                value={signupData.name}
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={text => handleInputChange('name', text)}
+              />
+              <TextInput
+                label="Email"
+                mode="outlined"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={signupData.email}
+                onChangeText={text => handleInputChange('email', text)}
+                style={{ marginTop: 8 }}
+              />
+              <TextInput
+                label="Password"
+                mode="outlined"
+                value={signupData.password}
+                secureTextEntry
+                onChangeText={text => handleInputChange('password', text)}
+                style={{ marginTop: 8 }}
+              />
+              <Button
+                dark
+                mode="contained"
+                loading={loading}
+                onPress={signupUser}
+                style={{ marginTop: 16 }}
+              >
+                Sign Up
+              </Button>
+              <Text
+                onPress={() => navigation.navigate('Login')}
+                style={{
+                  textAlign: 'center',
+                  marginTop: 16,
+                  textDecorationLine: 'underline',
+                }}
+              >
+                Already have an account ? Sign In.
+              </Text>
+            </Card.Content>
+          </Card>
+        </KeyboardAvoidingView>
       </ScrollView>
     </AuthLayout>
   );
